f09bfaebe854a7669e61c2f382214b1d2ad0da7c
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-12-05 11:23:15 +0100

    day1

---
+++ README.md
@@ -0,0 +1,3 @@
+# :christmas_tree: Advent of Code 2020 :christmas_tree:
+
+- [x] [Day 1: Report Repair](day-1/src/main.rs)
---
+++ day-1/src/main.rs
@@ -0,0 +1,64 @@
+use core::panic;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::{collections::HashSet, fs::File};
+
+fn read_input() -> HashSet<i32> {
+    let f = File::open("src/input.txt").expect("Failed to read input file!");
+    let f = BufReader::new(f);
+
+    return f.lines().fold(HashSet::new(), |mut acc, line| {
+        let value: i32 = line.unwrap().parse().unwrap();
+        acc.insert(value);
+        acc
+    });
+}
+
+fn part1() -> i32 {
+    let values = read_input();
+    for value in values.iter() {
+        let target: i32 = 2020 - value;
+        if values.contains(&target) {
+            return target * value;
+        }
+    }
+    panic!("Could not solve part 1");
+}
+
+fn part2() -> i32 {
+    let values = read_input();
+    for value in values.iter() {
+        let target: i32 = 2020 - value;
+        for another_value in values.iter() {
+            if another_value != value {
+                let another_target: i32 = target - another_value;
+                if values.contains(&another_target) {
+                    let last_value: i32 = 2020 - value - another_target;
+                    return value * another_target * last_value;
+                }
+            }
+        }
+    }
+
+    panic!("Could not solve part 2");
+}
+
+fn main() {
+    println!("Result part 1: {}", part1());
+    println!("Result part 2: {}", part2());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 877971);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 203481432);
+    }
+}
